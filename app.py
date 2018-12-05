df31415a362535053851047cd087b233adcded26
leavingink <42506677+leavingink@users.noreply.github.com>
2018-12-05 23:33:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, message)
 
 def Reply(text):
-    list = ["三民金城武", "高醫彭于晏", "呼吸孔劉"]
+    caballas = ["三民金城武", "高醫彭于晏", "呼吸孔劉"]
     if text.find("Hi")!=-1:
         return "ㄤㄤ"
     elif text.find("泓儒")!=-1:
-        return text = random.choice(list)
+        return text = random.choice(caballas)
     else:
         return text
 import os
