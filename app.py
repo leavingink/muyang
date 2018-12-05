fa90ba03ac5409c5247c736756e393ccd8302a54
leavingink <42506677+leavingink@users.noreply.github.com>
2018-12-06 01:33:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def Reply(text):
     caballas = ['三民金城武', '高醫彭于晏', '呼吸孔劉']
 	fallenleaf = ['61487', '8761']
     if text.find('泓儒')!=-1:
-        return random.choice(caballas)
-	elif text.find('殘楓落葉')!= -1:
+		return random.choice(caballas)
+	elif text.find('殘楓落葉')!=-1:
 		return random.choice(fallenleaf)
     else:
-        return text
+		return text
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
