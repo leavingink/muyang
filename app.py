cbd7b8fb04c9531ce54823f1499bb2abe335d0f1
leavingink <42506677+leavingink@users.noreply.github.com>
2018-12-05 23:42:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, message)
 
 def Reply(text):
-    caballas = ["三民金城武", "高醫彭于晏", "呼吸孔劉"]
+    caballas = ['三民金城武', '高醫彭于晏', '呼吸孔劉']
     if text.find("泓儒")!=-1:
         return text = random.choice(caballas)
     else:
