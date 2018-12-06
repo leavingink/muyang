ee19acbe9d04aa92e546b629dfae1fd786c6a59e
leavingink <42506677+leavingink@users.noreply.github.com>
2018-12-06 19:56:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def Button(event):
 		TemplateSendMessage(
 				alt_text='yeeee',
 				template=ButtonsTemplate(
-					thumbnail_image_uri='https://raw.githubusercontent.com/leavingink/muyang/master/2fdda3cc7cd98d10fa194dcd2a3fb80e7aec9079.jpg',
+					thumbnail_image_url='https://github.com/leavingink/muyang/blob/master/sheep.png?raw=true',
 					title='Eternal',
 					text='呼叫',
 					actions=[
